3cbcb67e23ccaa966f50cc0e9b6fa9e0b018ea5d
yogeshNavghane67 <yogeshnavghane661@gmail.com>
2023-12-20 18:07:06 +0530

    Post component

--- components/Feed.js
+++ components/Feed.js
@@ -1,4 +1,5 @@
 import { SparklesIcon } from "@heroicons/react/outline";
+import Post from "./Post";
 
 
 export default function Feed() {
@@ -27,10 +28,22 @@ return (
     <div className="xl:ml-[370px] border-l border-r border-gray-200  xl:min-w-[576px] sm:ml-[73px] flex-grow max-w-xl">
       <div className="flex py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200">
         <h2 className="text-lg sm:text-xl font-bold cursor-pointer">Home</h2>
-        * <div className="hoverEffect flex items-center justify-center px-0 ml-auto w-9 h-9">
+         <div className="hoverEffect flex items-center justify-center px-0 ml-auto w-9 h-9">
           <SparklesIcon className="h-5" />
         </div>
       </div>
+
+      {post.map((post) => (
+        //   <motion.div
+        //     key={post.id}
+        //     initial={{ opacity: 0 }}
+        //     animate={{ opacity: 1 }}
+        //     exit={{ opacity: 0 }}
+        //     transition={{ duration: 1 }}
+        //   >
+            <Post key={post.id}  post={post} />
+        //   </motion.div>
+        ))}
 
     </div>
 )
